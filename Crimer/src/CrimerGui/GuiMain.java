5a86a1573f8d2b9365988b689edfb5819c3323ac
mavrek <33359919+mavrek@users.noreply.github.com>
2018-04-22 23:26:18 -0500

    Update GuiMain.java

--- Crimer/src/CrimerGui/GuiMain.java
+++ Crimer/src/CrimerGui/GuiMain.java
@@ -6,13 +6,10 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.apache.http.client.fluent.Request;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 
-
 public class GuiMain extends Application {
-
     private Scene scene;
     GuiView guiView;
 
@@ -21,16 +18,14 @@ public class GuiMain extends Application {
      */
     public static void main(String[] args) throws IOException {
         String location = "baton+rouge";
-        String request  = Request.Get("https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyB06sE1R5EMBA4ysw5m-Gmk3PSlnenKaYE")
+        String request = Request.Get("https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyB06sE1R5EMBA4ysw5m-Gmk3PSlnenKaYE")
                 .connectTimeout(1000)
                 .socketTimeout(1000)
                 .execute().returnContent().asString();
 
         Object jsonString = Configuration.defaultConfiguration().jsonProvider().parse(request);
-
         String lat = JsonPath.read(jsonString, "results[0].geometry.location.lat").toString();
         String lng = JsonPath.read(jsonString, "results[0].geometry.location.lng").toString();
-
         PrintWriter writer = new PrintWriter("crimer/src/CrimerGui/latlong.js", "UTF-8");
         writer.println("var latCoord = " + lat + ";");
         writer.println("var longCoord = " + lng + ";");
@@ -41,12 +36,9 @@ public class GuiMain extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Crimer");
-
         guiView = new GuiView();
         scene = new Scene(guiView, 1000, 700);
-
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 }
